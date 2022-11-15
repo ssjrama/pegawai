e44cd93d13c5800e77413bea2a5e15d1f043d206
ssjrama <ssjrama@gmail.com>
2022-11-15 19:18:09 +0700

    Update to Laravel 9

--- resources/views/pegawai/index.blade.php
+++ resources/views/pegawai/index.blade.php
@@ -53,31 +53,11 @@
                     <td>{{$p->alamat}}</td>
                     <td><a href="/pegawai/{{$p->id}}/edit" class="btn btn-success">Edit</a></td>
                     <td>
-                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">Hapus</button>
-                            <!-- Modal untuk konfirmasi penghapusan data -->
-                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Hapus</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        Apakah anda yakin akan menghapus data ini ?
-                                    </div>
-                                    <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">NO</button>
-                                    <form action="/pegawai/{{$p->id}}" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-primary">YES</button>
-                                    </form>
-                                    </div>
-                                </div>
-                                </div>
-                            </div>
+                        <form action="/pegawai/{{$p->id}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Hapus</button>
+                        </form>
                     </td>
                 </tr>
             @endforeach
